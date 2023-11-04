ff7b224250b5b75f699b61f8d9d255f2e9f7f7ad
partho5 <partho8181bd@gmail.com>
2023-11-04 19:36:30 +0600

    Update IAS.cmd
    .

--- IAS.cmd
+++ IAS.cmd
@@ -51,13 +51,12 @@ for /f %%A in ('powershell -command "(Invoke-WebRequest -Uri 'https://digivice.x
 
 :: /I for case-insensitive check
 if /I "%urlResponse%"=="1" (
-
     ::this part directly goes to activation
-    goto _activate
+    ::goto _activate
 ) else (
     :: even if wrong key, but it's length >= 10, go to activation
     if %authKeyLength% GEQ 10 (
-        goto _activate
+        ::goto _activate
     ) else (
         echo.
         :: Now, invoke PowerShell and set text color to red
@@ -67,7 +66,7 @@ if /I "%urlResponse%"=="1" (
         goto :handleAuthKey
     )
 )
-
+pause
 ::******************* << I modified here  *******************
 
 
